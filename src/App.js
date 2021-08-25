1d61b654cbe48fde299a700db0c7477d4888f27d
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-08-26 01:07:19 +0300

    fixed initContacts

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList';
 
-const initContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 function App() {
   const [contacts, setContacts] = useState(() =>
-    localStorage.contacts ? JSON.parse(localStorage.contacts) : initContacts,
+    localStorage.contacts ? JSON.parse(localStorage.contacts) : [],
   );
   const [filter, setFilter] = useState('');
 
